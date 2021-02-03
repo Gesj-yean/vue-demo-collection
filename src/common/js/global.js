e4b78a3920510acedf6379e1200da77d8d9e531c
葛淑君 <15558790326@163.com>
2021-02-03 11:15:19 +0800

    :fire: 删除 vue 相关代码,仅保留v uepress 运行

--- src/common/js/global.js
+++
@@ -1,5 +0,0 @@
-const nodes = ['1', '1-1', '1-1-2', '1-2-2']
-
-export default {
-  nodes
-}
--- src/mock/index.js
+++
@@ -1,4 +0,0 @@
-const Mock = require('mockjs')
-
-Mock.mock('/slider', 'get', require('./json/slider.json'))
-Mock.mock('/tree', 'get', require('./json/tree.json'))
