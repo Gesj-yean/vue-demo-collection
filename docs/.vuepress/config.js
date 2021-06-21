321414d87899e039c6fe645b74f8ea9d1c46549f
葛淑君 <15558790326@163.com>
2021-06-21 20:16:44 +0800

    :sparkles:  vue页面离开拦截

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,7 +77,7 @@ module.exports = {
           sidebarDepth: 0,    // 可选的, 默认值是 1
           children: [
             ['/blog/ECMAScript2021', 'ECMAScript 2021新特性'],
-            ['/blog/custom-vuepress', 'VuePress 之自定义主题'],
+            ['/blog/page-exit-block', 'Vue 拦截离开页面操作'],
             ['/blog/edit-svg', '实现Vue 中可编辑的 SVG  Icon 组件'],
             ['/blog/encapsulate-components', '前端如何修改组件库源码来封装符合自己需求的组件？'],
           ]
@@ -90,6 +90,7 @@ module.exports = {
         sidebarDepth: 0,    // 可选的, 默认值是 1
         children: [
           ['/note/browser-compatibility', '浏览器兼容性记录2021-04-21 '],
+          ['/note/custom-vuepress', 'VuePress 之自定义主题'],
         ]
       },],
       '/essays/': [{
